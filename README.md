d6f6b5c98d533a438a4b07c6676e9cc00d7265d3
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-10-08 09:45:26 -0400

    added express handlebars views

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # eata-da-burger-ah
+
+
+### Heroku Deployment Steps
+
+Pre-Step: Run `heroku create` (takes an optional third argument)
+Pre-Step: Run `git remote -v` to check and make sure heroku is connected. 
+
+1. `git status`
+2. `git add .`
+3. `git commit -m `
+4. `git push heroku main` (assumes we're on the main branch, otherwise: `git push heroku branch:main`
+
+
--- server.js
+++ server.js
@@ -6,6 +6,19 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// VIEWS ROUTES
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// API ROUTES
+
 app.get("/api/config", (req, res) => {
   res.json({
     success: true,
